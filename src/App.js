72fd1daf4d2d3cd77528f7c2db25970b31035e93
charolia <sz500426@dal.ca>
2023-03-09 18:55:26 -0400

    Lab 6 code pushed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import Table from 'react-bootstrap/Table';
 
 function App() {
+  // Tabel referred from React Bootstrap
+  // URL: https://react-bootstrap.github.io/components/table/
+  // Date Accessed: 03/09/2023
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // Names generated from Squid
+    // URL: https://squid.org/rpg-random-generator
+    // Date Accessed: 03/09/2023
+    <div className="container">
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>First Name</th>
+          <th>Last Name</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>Stark</td>
+          <td>Altar</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>Guardian</td>
+          <td>Scrub</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td>Deeping</td>
+          <td>War</td>
+        </tr>
+	<tr>
+          <td>4</td>
+          <td>Abandoned</td>
+          <td>Mark</td>
+        </tr>
+	<tr>
+          <td>5</td>
+          <td>Old</td>
+          <td>Duchy</td>
+        </tr>
+      </tbody>
+    </Table>
     </div>
   );
 }
